e3249a76a90cbe2ed8b60ad91f04bb194d175a0b
Sean McDermott <smcdermo@chicagobooth.edu>
2017-07-17 17:37:31 +0000

    through payment form

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -3,7 +3,7 @@ class CalculationsController < ApplicationController
         #The incoming parameters for this actions look like {"a_number"=>"8"}
         #Rails stores that hash in a variable called params
         
-        @user_number = params["a_number"].to_i
+        @user_number = params["a_number"].to_f
         
         @squared_number = @user_number**2
         
@@ -12,7 +12,7 @@ class CalculationsController < ApplicationController
     
     def flex_square_root
         
-        @user_number = params["a_number"].to_i
+        @user_number = params["a_number"].to_f
         
         @square_root = @user_number**0.5
         
@@ -21,9 +21,9 @@ class CalculationsController < ApplicationController
     
     def flex_payment
         
-        @user_interest_rate = params["interest_rate"].to_i
+        @user_interest_rate = params["interest_rate"].to_f
         
-        @user_years = params["years"].to_i
+        @user_years = params["years"].to_f
         
         @user_principal = params["principal"].to_f
         
@@ -40,9 +40,9 @@ class CalculationsController < ApplicationController
     
     def flex_random
         
-        @user_low_end = params["low_end"].to_i
+        @user_low_end = params["low_end"].to_f
         
-        @user_high_end = params["high_end"].to_i
+        @user_high_end = params["high_end"].to_f
         
         @random_number = rand(@user_low_end .. @user_high_end)
         
@@ -61,11 +61,57 @@ class CalculationsController < ApplicationController
         #The incoming parameters for this actions look like {"the_user_number"=>"8"}
         #Rails stores that hash in a variable called params
         
-        @user_number = params["the_user_number"].to_i
+        @user_number = params["the_user_number"].to_f
         
         @squared_number = @user_number**2
         
         render("calculations/square_results_template.html.erb")
+    end
+    
+    def square_root_form
+        
+        
+        render("calculations/square_root_form_template.html.erb")
+    end
+    
+    def process_square_root
+        
+        #The incoming parameters for this actions look like {"the_user_number"=>"8"}
+        #Rails stores that hash in a variable called params
+        
+        @user_number = params["the_user_number"].to_f
+        
+        @square_root = @user_number**0.5
+        
+        render("calculations/square_root_results_template.html.erb")
+    end
+    
+    def payment_form
+        
+        
+        render("calculations/payment_form_template.html.erb")
+    end
+    
+    def process_payment
+        
+        #The incoming parameters for this actions look like {"the_user_number"=>"8"}
+        #Rails stores that hash in a variable called params
+        
+        @user_interest_rate = params["the_user_apr"].to_f
+        
+        @user_years = params["the_user_years"].to_f
+        
+        @user_principal = params["the_user_principal"].to_f
+        
+        @monthly_interest_rate = @user_interest_rate/100.000/12.000
+        
+        @period = @user_years*12
+        
+        @monthly_payment = (@monthly_interest_rate/(1-((1+@monthly_interest_rate)**-@period)))*@user_principal
+        
+        #@monthly_payment = (r/(1-((1+r)**-period)))*@principal
+        
+        render("calculations/payment_results_template.html.erb")
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,12 @@ Rails.application.routes.draw do
   get("/square/new", { :controller => "calculations", :action =>"square_form" })
   get("/square/results", { :controller => "calculations", :action =>"process_square" })
   
+  get("/square_root/new", { :controller => "calculations", :action =>"square_root_form" })
+  get("/square_root/results", { :controller => "calculations", :action =>"process_square_root" })  
+  
+  get("/payment/new", { :controller => "calculations", :action =>"payment_form" })
+  get("/payment/results", { :controller => "calculations", :action =>"process_payment" }) 
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
 end
